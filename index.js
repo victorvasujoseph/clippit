5c1aed6779bad8de7108bdc2356937413cc3ed78
Victor Joseph <9651630+victorvasujoseph@users.noreply.github.com>
2019-03-06 20:04:25 -0800

    Fixed Package.json - Both Client & Serve

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -15,8 +15,15 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/clippit");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/clippit");
 
 // Start the API server
 app.listen(PORT, function () {
