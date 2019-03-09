a614f32f85a2f82a605a0a939c46a4c3480ca00b
Victor Joseph <9651630+victorvasujoseph@users.noreply.github.com>
2019-03-09 14:18:10 -0800

    React Date Picker - Boiler Plate

--- client/src/components/pages/Book.js
+++ client/src/components/pages/Book.js
@@ -1,30 +1,29 @@
-import React from "react";
-import DatePicker from 'react-datepicker';
-import moment from 'moment';
+import React, { Component } from "react";
+// import { Card, CardGroup, ListGroupItem, ListGroup  } from 'react-bootstrap';
+import DatePicker from "react-datepicker";
+import addMonths from "date-fns/addMonths";
 
-
-class Book extends React.Component {
-
+class Book extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       startDate: new Date()
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-  };
+  }
 
   handleChange(date) {
     this.setState({
       startDate: date
-    })
-  };
+    });
+  }
 
   handleSubmit(e) {
     e.preventDefault();
-    let main = this.state.startDate
-    console.log(main.format('L'));
-  };
+    let main = this.state.startDate;
+    console.log(main.format("L"));
+  }
 
   render() {
     return (
@@ -33,21 +32,24 @@ class Book extends React.Component {
         <h3>Pick a date that best fit your schedule!</h3>
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
-            <label>Select Date:  </label>
+            <label>Select Date: </label>
             <DatePicker
               selected={this.state.startDate}
               onChange={this.handleChange}
-              showTimeSelect
-              name="startDate"
-            // dateFormat="MM/DD/YYYY"
+              minDate={new Date()}
+              maxDate={addMonths(new Date(), 5)}
+              showDisabledMonthNavigation
+              showMonthDropdown
+              withPortal
+              // name="startDate"
+              // dateFormat="MM/DD/YYYY"
             />
           </div>
           <div className="form-group">
-            <button className="btn btn-success">Add Date</button>
+            <button className="btn btn-danger">Add Date</button>
           </div>
         </form>
       </div>
-
     );
   }
 }
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,7 +3,6 @@ const UserSession = require("../../models/userSession");
 const router = require("express").Router();
 const authController = require("../../controller/authController");
 
-
 router.route("/account/signup").post(authController.signUp);
 router.route("/account/signin").post(authController.signIn);
 router.route("/account/verify").get(authController.verify);
