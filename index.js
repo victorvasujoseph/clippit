20ada37a31ce026d55de33ec95028c0c3f50dcf8
Victor Joseph <9651630+victorvasujoseph@users.noreply.github.com>
2019-03-09 13:53:49 -0800

    Login Temp Working

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use(routes);
 
 // Send every request to the React app
 // Define any API routes before this runs
-// app.get("*", function (req, res) {
-//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 
 // Connect to the Mongo DB
