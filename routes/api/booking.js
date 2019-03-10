26ae71e3d6d19b00bc30682faceb5fd98aeba93e
Victor Joseph <9651630+victorvasujoseph@users.noreply.github.com>
2019-03-10 11:59:28 -0700

    Get All Stylist

--- routes/api/booking.js
+++ routes/api/booking.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
-const bookingController = require('../../controller/bookingController')
+const bookingController = require("../../controller/bookingController");
 
-router.route('/stylist-available/:day/:month/:year')
-    .get(bookingController.getAvailableStylist);
+//get all stylist from the System for a specific date
+router
+  .route("/stylist-available/:day/:month/:year")
+  .get(bookingController.getAvailableStylist);
+//add a new stylist to the System
+router.route("/add-new-stylist").post(bookingController.addNewStylist);
+//get all stylist from the System
+router.route("/stylist").get(bookingController.stylist);
 
 module.exports = router;
