14d61624843dd36c370460e835380ea005711679
tteenz <trantiinaa@yahoo.com>
2019-03-18 04:02:20 -0700

    login auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
+import { LoginContainer, Alert, Redirect, Header } from "react-bootstrap"
+import Navbar from "./components"
 import Home from "./components/pages/Home";
 import Logout from "./components/pages/Logout";
 import Appointments from "./components/pages/Appointments";
@@ -10,21 +12,47 @@ import Gallery from "./components/pages/Gallery";
 import 'react-datepicker/dist/react-datepicker.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <NavTabs />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/logout" component={Logout} />
-        <Route exact path="/Appointments" component={Appointments} />
-        <Route exact path="/Book" component={Book} />
-        <Route exact path="/Gallery" component={Gallery} />
-        <Route exact path="/" component={Login} />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAlertOpen: false,
+    };
+    this.toggleAlert = this.toggleAlert.bind(this);
+  }
 
-      </div>
-    </Router>
-  );
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Route exact path="/(login)"  />
+          <Route component={LoginContainer} />
+
+        </div>
+
+
+        <div className="container">
+          <Route exact path="/" render={() => <Redirect to="/login" />} />
+          <Route path="/login" component={Login} />
+          <Route exact path="/Gallery" component={Gallery} />
+
+        </div>
+
+        <Header toggleAlert={this.toggleAlert} />
+        <div className="container">
+          <Navbar />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/logout" component={Logout} />
+          <Route exact path="/Appointments" component={Appointments} />
+          <Route exact path="/Book" component={Book} />
+          <Route exact path="/Gallery" component={Gallery} />
+          {this.state.isAlertOpen ? <Alert /> : null}
+
+        </div>
+</Router >
+    );
+  }
 }
 
 export default App;
--- client/src/components/pages/Book.js
+++ client/src/components/pages/Book.js
@@ -446,15 +446,7 @@ class Book extends Component {
               <p>You Have Selected the Following Slot for your Appointment</p>
               <ListGroup variant="flush">
                 <ListGroup.Item>
-                  Date : {moment(this.state.startDate).format("DD")}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  {" "}
-                  Month : {moment(this.state.startDate).format("MM")}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  {" "}
-                  Year : {moment(this.state.startDate).format("YYYY")}
+                  Date :  {moment(this.state.startDate).format("MM")}/{moment(this.state.startDate).format("DD")}/{moment(this.state.startDate).format("YYYY")}
                 </ListGroup.Item>
                 <ListGroup.Item>
                   Time : {this.state.selectedTimeSlot}
