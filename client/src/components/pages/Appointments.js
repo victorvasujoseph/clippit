6478220a91bffcb815d39db0ecd4966e9bf29e54
Victor Joseph <9651630+victorvasujoseph@users.noreply.github.com>
2019-03-18 12:27:06 -0700

    Upcoming Appointments

--- client/src/components/pages/Appointments.js
+++ client/src/components/pages/Appointments.js
@@ -29,11 +29,46 @@ class Appointment extends Component {
             fetch("/api/booking/customer-schedule/" + customerID)
               .then(response => response.json())
               .then(data => {
-                console.log(data);
+                
+                var listX = data.map(value => {
+                  
+                  switch (value.timeSlot) {
+                    case "slot1":
+                      value.timeSlot = "8:00 AM";
+                      break;
+                    case "slot2":
+                      value.timeSlot = "9:00 AM";
+                      break;
+                    case "slot3":
+                      value.timeSlot = "10:00 AM";
+                      break;
+                    case "slot4":
+                      value.timeSlot = "11:00 AM";
+                      break;
+                    case "slot5":
+                      value.timeSlot = "1:00 PM";
+                      break;
+                    case "slot6":
+                      value.timeSlot = "2:00 PM";
+                      break;
+                    case "slot7":
+                      value.timeSlot = "3:00 PM";
+                      break;
+                    case "slot8":
+                      value.timeSlot = "4:00 PM";
+                      break;
+                    default:
+                      value.timeSlot = "";
+                      break;
+                  }
+                  return value;
+                })
+                
+
                 this.setState({
-                  appointmentList: data
+                  appointmentList: listX
                 });
-              }); 
+              });
           } else {
             this.setState({
               isLoggedIn: false
@@ -60,13 +95,23 @@ class Appointment extends Component {
               <th>Service Provider</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td />
-              <td />
-              <td />
-            </tr>
-          </tbody>
+          {this.state.appointmentList.map(value => {
+            return (
+              <tbody>
+                <tr>
+                  <td>
+                    {value.month +
+                      "/" +
+                      value.day +
+                      "/" +
+                      value.year}
+                  </td>
+                  <td>{value.timeSlot}</td>
+                  <td>{value.stylistID}</td>
+                </tr>
+              </tbody>
+            );
+          })}
         </Table>
 
         <ButtonGroup className="mr-2" aria-label="First group">
